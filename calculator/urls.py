b1c42e5dc1f46236757c0d40ee286a6dbdf8aa5b
tlarnold10 <tlarnold10@hotmail.com>
2020-05-09 16:53:19 -0500

    added new a covid data and progress tool

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,10 +1,16 @@
 
 from django.conf.urls import url
 
-from .views import CalculatorApp
+from .views import CalculatorApp, ProgressBar, covidData
 
 urlpatterns = [
-	url(r'^$',
+	url(r'^calculator$',
 		CalculatorApp.as_view(),
-		name='emotion_list'),
+		name='calculator'),
+	url(r'^progress$',
+		ProgressBar.as_view(),
+		name='prgress_bar'),
+	url(r'^covid$',
+		covidData.as_view(),
+		name='covid'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, CreateView, DetailView
 
+import random
+import requests
+import pandas as pd
+import pdb
+
 class CalculatorApp(View):
 	template_name = 'calculator/calc_app.html'
 
 	def get(self, request):
 		return render(request, self.template_name)
+
+class ProgressBar(View):
+	template_name = 'calculator/progress.html'
+	progress = random.randint(1, 100)
+	total = 100
+	context = {'progress': progress, 'total':total}
+
+	def get(self, request):
+		return render(request, self.template_name, self.context)
+
+class covidData(View):
+	url = "https://covidtracking.com/api/v1/states/daily.json"
+	response = requests.get(url)
+	json_data = response.json()
+	template_name = 'calculator/covid.html'
+	covid_df = pd.DataFrame(json_data)
+	pdb.set_trace()
+	context = {'data':covid_df}
+	
+	def get(self, request):
+		return render(request, self.template_name, self.context)
